09a869b2caaa3201cba7e86cd6ce215d9b50a4dc
esoytesoy123-oss <esoytesoy123@gmail.com>
2026-03-09 23:52:23 +0800

    Add app.tsx for financial tracking app

---
+++ app.tsx
@@ -0,0 +1,13 @@
+// This is a React Native financial tracking app
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Financial Tracking App</Text>
+    </View>
+  );
+};
+
+export default App;
